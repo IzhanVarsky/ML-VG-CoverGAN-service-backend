d098af2818b208a94fbfabc53aa39e80d446a17b
IzhanVarsky <48652648+IzhanVarsky@users.noreply.github.com>
2022-09-13 18:31:55 +0300

    covergan backend port changed

--- covergan_backend/docker_run_covergan_backend.sh
+++ covergan_backend/docker_run_covergan_backend.sh
@@ -1,1 +1,1 @@
-docker run --shm-size=2g --rm -p 1123:1123 -v `pwd`:/scratch --user $(id -u):$(id -g) --workdir=/scratch -e HOME=/scratch covergan_backend:latest
+docker run --shm-size=2g --restart=always -p 9013:9013 -v `pwd`:/scratch --user $(id -u):$(id -g) --workdir=/scratch -e HOME=/scratch covergan_backend:latest
