2d16ab353f8910ab82bd828547b20944ce00ffff
IzhanVarsky <48652648+IzhanVarsky@users.noreply.github.com>
2022-10-02 02:52:13 +0300

    diffvg optim tempfile bug fixed

--- covergan_backend/diffvg_optim.py
+++ covergan_backend/diffvg_optim.py
@@ -3,8 +3,8 @@ import tempfile
 import pydiffvg
 
 
-def run_diffvg_optim(svg):
+def run_diffvg_optim(svg: str):
     with tempfile.NamedTemporaryFile(suffix='.svg', delete=True) as f:
-        f.write(svg)
+        f.write(svg.encode("utf-8"))
         x = pydiffvg.svg_to_scene(f.name)
         return pydiffvg.svg_to_str(*x)
